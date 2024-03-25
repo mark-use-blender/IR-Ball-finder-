dde606b034609a153473d101f9d803d73c7a80bf
mark-use-blender <markyoutub00@gmail.com>
2024-03-25 10:48:39 +0800

    bump

--- main.py
+++ main.py
@@ -3,24 +3,29 @@ import numpy as np
 import math as m
 
 def daytime():
-
-
+    #gpio=HIGH
     return
-
-
 def nightime():
-
-
+    #gpio=LOW
     return
 def go_up():
+    #goup
     return
 def go_down():
+    #godown
     return
 def go_left():
+    #goleft
     return
 def go_right():
+    #goright
     return
+def no_track():
+    #notrack
+    return
+
 cam = cv2.VideoCapture(2)
+
 def find_cir(con):
     balllist = []
     if (len(con)>0):
@@ -33,20 +38,14 @@ def find_cir(con):
     return balllist
 
 while (True):
-
     daytime()
     ret,dayimg= cam.read()
     nightime()
     ret,nighting=cam.read()
-    grayday = cv2.cvtColor(dayimg,cv2.COLOR_BGR2GRAY)\
-    
+    grayday = cv2.cvtColor(dayimg,cv2.COLOR_BGR2GRAY)
     graynight = cv2.cvtColor(nighting,cv2.COLOR_BGR2GRAY)
-
-
-
     adapt_type = cv2.ADAPTIVE_THRESH_GAUSSIAN_C
     thresh_type = cv2.THRESH_BINARY_INV
-
     diff = cv2.bitwise_xor(grayday,graynight)
     blur = cv2.medianBlur(diff, 1)
     contress = cv2.adaptiveThreshold(blur, 255, adapt_type, thresh_type, 21, 7)
@@ -55,24 +54,25 @@ while (True):
     lar = 0
     lpos = [0,0]
     h,w = diff.shape
-
     balllist = find_cir(con)
-
-    if (len(balllist)>0):
+    tracking = (len(balllist)>0)
+    if (tracking):
         balllist=balllist[balllist[:, 1].argsort()]
         lcon,lar,lpos=balllist[0]
 
-    apos = lpos[0]-(h/2),lpos[1]-(w/2)
-    if (abs(apos[0])>20):
-        if (apos[0]<0):
-            go_down()
-        else:
-            go_up()
-    if (abs(apos[1])>20):
-        if (apos[1]<0):
-            go_left()
-        else:
-            go_right()
+        apos = lpos[0]-(h/2),lpos[1]-(w/2)
+        if (abs(apos[0])>20):
+            if (apos[0]<0):
+                go_down()
+            else:
+                go_up()
+        if (abs(apos[1])>20):
+            if (apos[1]<0):
+                go_left()
+            else:
+                go_right()
+    else:
+        no_track()
 
     
 
